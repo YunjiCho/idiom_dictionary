f4a59f7897e11d77521009e509bcdda493ac328f
YunjiCho <76531885+YunjiCho@users.noreply.github.com>
2023-06-13 16:18:53 +0900

    스크롤에 따른 이미지의 y좌표 움직이게

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var headings = document.querySelectorAll("h2");
+var mainImage = document.getElementById("mainImage");
+var isScrolling = false;
+var lastClickTime = 0;
 
 headings.forEach(function (heading) {
   var content = heading.nextElementSibling;
@@ -7,20 +10,37 @@ headings.forEach(function (heading) {
   content.style.display = "none";
 
   heading.addEventListener("click", function () {
-    if (content.style.display === "none") {
-      content.style.display = "flex";
-      textContainer.style.flexDirection = "column";
-      // heading.classList.toggle("h2-active");
-      changeImage(heading.getAttribute("data-image"), this);
+    var currentTime = new Date().getTime();
+    var elapsedTime = currentTime - lastClickTime;
 
+    if (elapsedTime > 20000) {
+      location.reload();
     } else {
-      content.style.display = "none";
-      // heading.classList.toggle("h2-active");
+      if (content.style.display === "none") {
+        content.style.display = "flex";
+        textContainer.style.flexDirection = "column";
+        changeImage(heading.getAttribute("data-image"), this);
+      } else {
+        content.style.display = "none";
+      }
     }
+
+    lastClickTime = currentTime;
   });
 });
 
 function changeImage(imageName, element) {
-  var mainImage = document.getElementById("mainImage");
   mainImage.src = imageName;
 }
+
+window.addEventListener("scroll", function () {
+  if (!isScrolling) {
+    window.requestAnimationFrame(function () {
+      if (window.innerWidth > 768) {
+        mainImage.style.top = window.pageYOffset + "px";
+      }
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
+});
