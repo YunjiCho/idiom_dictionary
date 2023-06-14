a7200a4adc974bc4dab0a739cd0fbce3986d8913
YunjiCho <76531885+YunjiCho@users.noreply.github.com>
2023-06-14 13:03:35 +0900

    Mobile image y

--- script.js
+++ script.js
@@ -47,3 +47,34 @@ window.addEventListener("scroll", function () {
   }
   isScrolling = true;
 });
+
+if (window.innerWidth <= 768) {
+  var targetDiv = document.querySelector(".text-container2");
+
+  headings.forEach(function (heading) {
+    var content = heading.nextElementSibling;
+    var textContainer = content.querySelector("div");
+
+    heading.addEventListener("click", function () {
+      if (content.style.display === "none") {
+        headings.forEach(function (h) {
+          h.nextElementSibling.style.display = "none";
+        });
+        content.style.display = "flex";
+        textContainer.style.flexDirection = "column";
+        changeImage(heading.getAttribute("data-image"));
+
+        // 이미지 위치를 클릭한 h2 바로 위로 조정
+        mainImage.style.position = "relative";
+        mainImage.style.top = "-100%";
+      } else {
+        content.style.display = "none";
+        mainImage.style.position = "";
+        mainImage.style.top = "";
+      }
+
+      headings.forEach((h) => h.classList.remove("highlight"));
+      heading.classList.add("highlight");
+    });
+  });
+}
